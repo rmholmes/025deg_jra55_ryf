89c28c3c56ed78cd28d68986970b7e001aa7b4f8
Ryan Holmes <holmes.ryan.m@gmail.com>
2020-07-17 17:29:41 +1000

    2020-07-17 17:29:40: Run 84

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -2,7 +2,7 @@
 #PBS -q copyq
 #PBS -l ncpus=1
 #PBS -l wd
-#PBS -l walltime=1:00:00,mem=4GB
+#PBS -l walltime=4:00:00,mem=4GB
 #PBS -P v45
 #PBS -N output_to_gdata
 #PBS -l storage=gdata/hh5+gdata/ik11+scratch/e14+gdata/ua8
@@ -10,7 +10,7 @@
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
 # Make a unique path for your set of runs.
 # DOUBLE-CHECK IT IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
-GDATADIR=/g/data/ik11/outputs/access-om2-025/025deg_jra55_ryf9091_gadi/
+GDATADIR=/g/data/ik11/outputs/access-om2-025/025deg_jra55_ryf9091_gadi_norediGM/
 
 mkdir -p ${GDATADIR}
 cd archive
